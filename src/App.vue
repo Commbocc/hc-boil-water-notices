02f07b7c84b229d0ca0121f3bc006fde48e5ade1
Kevin McCarthy <mccarthyk@hcflgov.net>
2021-02-04 16:19:47 -0500

    init component app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="table-repsonive">
+    <table class="table table-striped table-bordered table-condensed">
+      <thead>
+        <tr>
+          <th
+            v-for="header in [
+              'Area',
+              'Effective',
+              'Rescinded',
+              'Press Release',
+              'Rescinsion Release',
+              'Last Updated',
+            ]"
+            :key="header"
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="{ id, fields: record } in records" :key="id">
+          <td v-text="record.area" />
+
+          <td v-text="new Date(record.effectiveAt).toLocaleString()" />
+
+          <td>
+            <template v-if="record.rescinded">
+              <strong>Yes</strong>, this notice is no longer in effect
+            </template>
+            <template v-else>
+              <strong>No</strong>, this notice is in effect
+            </template>
+          </td>
+
+          <td>
+            <a v-if="record.releaseUrl" :href="record.releaseUrl"
+              >Press Release</a
+            >
+            <span v-else>
+              -
+            </span>
+          </td>
+
+          <td>
+            <a
+              v-if="record.rescinsionReleaseUrl"
+              :href="record.rescinsionReleaseUrl"
+              >Rescinsion Release</a
+            >
+            <span v-else>
+              -
+            </span>
+          </td>
+
+          <td v-text="new Date(record.updatedAt).toLocaleString()" />
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  install(Vue) {
+    Vue.prototype.$airtable = require('./airtable')
+    Vue.component('HcBoilWaterNotices', this)
+  },
+
+  data: () => ({
+    records: [],
+  }),
+
+  async mounted() {
+    try {
+      // https://airtable.com/appPNfSpl4rulv7fg/api/docs#curl/table:notices:list
+      const { data } = await this.$airtable.get(`/notices`, {
+        params: {
+          view: 'Grid view',
+        },
+      })
+      this.records = data.records
+    } catch (error) {
+      // handle error
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue/dist/vue'
+import HcBoilWaterNotices from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(HcBoilWaterNotices)
+
 new Vue({
-  render: h => h(App),
+  // render: (h) => h(App),
 }).$mount('#app')
